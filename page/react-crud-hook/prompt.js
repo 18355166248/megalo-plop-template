26cbf4e72d0469b783d2fc2688905b70fe9e2578
SMegalo <lang.jiang@ximalaya.com>
2023-05-06 11:15:15 +0800

    feat: 更新提示

--- page/react-crud-hook/prompt.js
+++ page/react-crud-hook/prompt.js
@@ -1,4 +1,4 @@
-const utils = require('../utils');
+const utils = require("../utils");
 
 function getPath(pathStr) {
   const rootPath = process.cwd();
@@ -6,27 +6,28 @@ function getPath(pathStr) {
 }
 
 module.exports = {
-  description: '生成一个带 增删改查功能的 react 状态管理为 hook 的代码文件夹', // 描述这个generate的作用
+  description: "生成一个带 增删改查功能的 react 状态管理为 hook 的代码文件夹", // 描述这个generate的作用
   prompts: [
     {
-      type: 'input', // 问题的类型
-      name: 'pathName', // 问题对应得到答案的变量名，可以在acitons中使用该变量
-      message: '文件路径+名称( 默认创建在文件夹下 )', // 在命令行中的问题
-      validate: name => !!name.trim(),
+      type: "input", // 问题的类型
+      name: "pathName", // 问题对应得到答案的变量名，可以在acitons中使用该变量
+      message:
+        "默认在 src/pages 下创建, 请输入需要创建的路径或者文件夹名 输入例: One/Two 或 Two即可", // 在命令行中的问题
+      validate: (name) => !!name.trim(),
     },
     {
-      type: 'input', // 问题的类型
-      name: 'ZHName', // 问题对应得到答案的变量名，可以在acitons中使用该变量
-      message: '中文名', // 在命令行中的问题
-      validate: name => !!name.trim(),
+      type: "input", // 问题的类型
+      name: "ZHName", // 问题对应得到答案的变量名，可以在acitons中使用该变量
+      message: "中文名", // 在命令行中的问题
+      validate: (name) => !!name.trim(),
     },
   ],
-  actions: data => {
+  actions: (data) => {
     // 这里可以通过data获取输入的pathname
-    const name = data.pathName.split('/');
+    const name = data.pathName.split("/");
     const LastName = name[name.length - 1];
     LastName[0] = LastName[0].toLocaleUpperCase();
-    const excuteLastName = name.slice(0, -1).join('/');
+    const excuteLastName = name.slice(0, -1).join("/");
 
     const firstUpperCase = utils.setFirstUpperCase(LastName); // 文件名
     const path = excuteLastName ? `/${excuteLastName}` : excuteLastName; // 路径
@@ -35,9 +36,9 @@ module.exports = {
 
     const actions = [
       {
-        type: 'add', // 操作类型 添加文件
+        type: "add", // 操作类型 添加文件
         path: getPath(`${path}${`/${LastName}`}/index${firstUpperCase}.tsx`), // 添加的文件的路径
-        templateFile: 'page/react-crud-hook/template/index.hbs',
+        templateFile: "page/react-crud-hook/template/index.hbs",
         data: {
           name: LastName,
           firstUpperName: firstUpperCase,
@@ -45,9 +46,9 @@ module.exports = {
         },
       },
       {
-        type: 'add', // 操作类型 添加文件
+        type: "add", // 操作类型 添加文件
         path: getPath(`${path}${`/${LastName}`}/Table${firstUpperCase}.tsx`), // 添加的文件的路径
-        templateFile: 'page/react-crud-hook/template/Table.hbs',
+        templateFile: "page/react-crud-hook/template/Table.hbs",
         data: {
           name: LastName,
           firstUpperName: firstUpperCase,
@@ -55,9 +56,9 @@ module.exports = {
         },
       },
       {
-        type: 'add', // 操作类型 添加文件
+        type: "add", // 操作类型 添加文件
         path: getPath(`${path}${`/${LastName}`}/${LastName}.context.ts`), // 添加的文件的路径
-        templateFile: 'page/react-crud-hook/template/index.context.hbs',
+        templateFile: "page/react-crud-hook/template/index.context.hbs",
         data: {
           name: LastName,
           firstUpperName: firstUpperCase,
@@ -65,9 +66,9 @@ module.exports = {
         },
       },
       {
-        type: 'add', // 操作类型 添加文件
+        type: "add", // 操作类型 添加文件
         path: getPath(`${path}${`/${LastName}`}/${LastName}.reducer.tsx`), // 添加的文件的路径
-        templateFile: 'page/react-crud-hook/template/index.reducer.hbs',
+        templateFile: "page/react-crud-hook/template/index.reducer.hbs",
         data: {
           name: LastName,
           firstUpperName: firstUpperCase,
@@ -75,11 +76,11 @@ module.exports = {
         },
       },
       {
-        type: 'add', // 操作类型 添加文件
+        type: "add", // 操作类型 添加文件
         path: getPath(
           `${path}${`/${LastName}`}/FilterForm${firstUpperCase}.tsx`
         ), // 添加的文件的路径
-        templateFile: 'page/react-crud-hook/template/FilterForm.hbs',
+        templateFile: "page/react-crud-hook/template/FilterForm.hbs",
         data: {
           name: LastName,
           firstUpperName: firstUpperCase,
@@ -87,11 +88,11 @@ module.exports = {
         },
       },
       {
-        type: 'add', // 操作类型 添加文件
+        type: "add", // 操作类型 添加文件
         path: getPath(
           `${path}${`/${LastName}`}/DetailModal${firstUpperCase}.tsx`
         ), // 添加的文件的路径
-        templateFile: 'page/react-crud-hook/template/DetailModal.hbs',
+        templateFile: "page/react-crud-hook/template/DetailModal.hbs",
         data: {
           name: LastName,
           firstUpperName: firstUpperCase,
@@ -99,9 +100,9 @@ module.exports = {
         },
       },
       {
-        type: 'add', // 操作类型 添加文件
+        type: "add", // 操作类型 添加文件
         path: getPath(`${path}${`/${LastName}`}/constant${firstUpperCase}.tsx`), // 添加的文件的路径
-        templateFile: 'page/react-crud-hook/template/constant.hbs',
+        templateFile: "page/react-crud-hook/template/constant.hbs",
         data: {},
       },
     ];
