8e72efcc1e8ca2981f9b649efa0d093fc4cbd70b
“jianglang” <lang.jiang@ximalaya.com>
2024-02-21 16:28:05 +0800

    feat: 新增react-crud-mi-design

---
+++ page/react-crud-mi-design/README.md
@@ -0,0 +1,1 @@
+# 基于[mi-design](http://cft.pages.xmly.work/mi-design#/components/filter-form-layout) 开发的一个 CRUD 组件
--- plopfile.js
+++ plopfile.js
@@ -1,7 +1,9 @@
 const reactCRUDHookGenerator = require("./page/react-crud-hook/prompt");
 const reactCRUDMobxGenerator = require("./page/react-crud-mobx/prompt");
+const reactCRUDMiDesignGenerator = require("./page/react-crud-mi-design/prompt");
 
 module.exports = function (plop) {
   plop.setGenerator("react-curd-hook", reactCRUDHookGenerator);
   plop.setGenerator("react-curd-mobx", reactCRUDMobxGenerator);
+  plop.setGenerator("react-curd-mi-design", reactCRUDMiDesignGenerator);
 };
